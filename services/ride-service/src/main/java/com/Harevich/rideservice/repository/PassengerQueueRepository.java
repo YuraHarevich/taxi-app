3197d386e64df2e7fd18ec239c034f9319d6e7cb
Best Juare <burdance2004@gmail.com>
2025-02-21 01:45:17 +0300

    done with exception handling

--- services/ride-service/src/main/java/com/Harevich/rideservice/repository/PassengerQueueRepository.java
+++ services/ride-service/src/main/java/com/Harevich/rideservice/repository/PassengerQueueRepository.java
@@ -4,11 +4,17 @@ import com.Harevich.rideservice.model.PassengerQueueElement;
 import com.Harevich.rideservice.model.enumerations.ProcessingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface PassengerQueueRepository extends JpaRepository<PassengerQueueElement, UUID> {
 
     Optional<PassengerQueueElement> findFirstByProcessingStatusOrderByCreatedAtAsc(ProcessingStatus processingStatus);
+
+    List<PassengerQueueElement> findByCreatedAtBefore(LocalDateTime dateTime);
+
+    List<PassengerQueueElement> findByProcessingStatus(ProcessingStatus status);
 
 }
