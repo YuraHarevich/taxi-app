b6dcd745a990d59d729587a657691b8a62fbe94c
Best Juare <burdance2004@gmail.com>
2025-03-31 15:13:30 +0300

    fixed trace difference in feign

--- services/ride-service/src/main/java/com/kharevich/rideservice/client/passenger/PassengerServiceClientDev.java
+++ services/ride-service/src/main/java/com/kharevich/rideservice/client/passenger/PassengerServiceClientDev.java
@@ -1,13 +1,14 @@
 package com.kharevich.rideservice.client.passenger;
 
 import com.kharevich.rideservice.controller.exceptions.PassengerServiceErrorDecoder;
+import com.kharevich.rideservice.util.config.FeignConfig;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Profile;
 
 @FeignClient(
         name = "passenger-service",
-        configuration = PassengerServiceErrorDecoder.class
+        configuration = {PassengerServiceErrorDecoder.class, FeignConfig.class}
 )
 public interface PassengerServiceClientDev extends PassengerServiceClient {
 }
--- services/ride-service/src/main/java/com/kharevich/rideservice/util/config/FeignConfig.java
+++ services/ride-service/src/main/java/com/kharevich/rideservice/util/config/FeignConfig.java
@@ -1,13 +1,14 @@
 package com.kharevich.rideservice.util.config;
 
-import feign.Logger;
 import org.springframework.context.annotation.Bean;
 
+import feign.RequestInterceptor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class FeignConfig {
-
     @Bean
-    Logger.Level feignLoggerLevel() {
-        return Logger.Level.FULL;
+    public RequestInterceptor feignTracingInterceptor() {
+        return new TracingFeignInterceptor();
     }
-
 }
