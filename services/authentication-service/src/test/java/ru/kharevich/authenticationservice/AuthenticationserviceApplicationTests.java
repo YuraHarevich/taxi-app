517899da1a5cef2285f644279ef8af238a6f925e
Best Juare <burdance2004@gmail.com>
2025-04-02 11:14:37 +0300

    added auth service

---
+++ services/authentication-service/src/test/java/ru/kharevich/authenticationservice/AuthenticationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kharevich.authenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
